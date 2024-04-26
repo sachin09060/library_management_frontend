b25982fbccda4fb534ac90a95ad14c79eb81953c
Dilipraj10 <dilipraj5655@gmail.com>
2024-04-26 21:36:37 +0530

    Userdashboard

--- library/src/App.js
+++ library/src/App.js
@@ -2,18 +2,22 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Componets/Dashbord/Home';
-import Dashbord from './Componets/Dashbord/Drawer2'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AdminSignIn from './Componets/login/AdminSignIn';
-import UserSignIn from './Componets/login/SignIn';
-import RegisterPage from './Componets/login/RegisterPage';
-import ForgotPassword from './Componets/login/ForgotPassword';
+// import Home from './Componets/Dashbord/Home';
+// import Dashbord from './Componets/Dashbord/Drawer2'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import AdminSignIn from './Componets/login/AdminSignIn';
+// import UserSignIn from './Componets/login/SignIn';
+// import RegisterPage from './Componets/login/RegisterPage';
+// import ForgotPassword from './Componets/login/ForgotPassword';
+// import UserNavBar from './Componets/UserDashboard/Cards/UserNavBar'
+// import UserDetails from './Componets/UserDashboard/Cards/UserDetails'
+// import Books from './Componets/UserDashboard/Cards/Books'
+import GridBooks from './Componets/UserDashboard/Cards/BooksPage'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/admin' element={<Dashbord />} />
@@ -22,7 +26,11 @@ function App() {
           <Route path='/UserSignUp' element={<RegisterPage />} />
           <Route path='/Forgot' element={<ForgotPassword />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      {/* <UserNavBar/> */}
+      {/* <UserDetails/> */}
+      {/* <Books/> */}
+      <GridBooks/>
     </div>
   );
 }
